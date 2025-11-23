d9ec81b0fd9a046fbabe02ed9fd29f71e15f0d12
minhi0449 <minhi0449@gmail.com>
2025-11-24 00:20:05 +0900

    서블릿 작성/ web.xml 수정

---
+++ BoardWeb/src/main/java/com/ssamz/web/user/HttpServletRequest.java
@@ -0,0 +1,5 @@
+package com.ssamz.web.user;
+
+public class HttpServletRequest {
+
+}
---
+++ BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.ssamz.web.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(name = "LoginProcess", urlPatterns = { "/LoginProcess" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    	System.out.println("===> LoginServlet 생성");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("---> GET 방식의 요청 처리");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println("---> POST 방식의 요청 처리");
+	}
+
+}
