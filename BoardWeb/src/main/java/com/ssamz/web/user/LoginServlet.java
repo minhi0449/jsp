e2e82fe5cbcb1fa960404c0d52ade75891706da4
minhi0449 <minhi0449@gmail.com>
2025-12-01 22:17:00 +0900

    로그인 처리를 위한 서블릿 클래스 추가

--- BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
+++ BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -11,28 +11,32 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-//    public LoginServlet() {
-//    	System.out.println("===> LoginServlet 생성");
-//    }
+    public LoginServlet() {
+    	System.out.println("===> LoginServlet 생성");
+    }
 	
 	public LoginServlet(String servletName) {
     	System.out.println("===> " + servletName + "생성");
     }
 
-	//서블릿 콜백 메소드 생성
 	public void init() throws ServletException {
 		System.out.println("---> init 호출");
 	}
 	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("---> service() 호출");
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("---> GET 방식의 요청 처리");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// doGet(request, response);
-		// System.out.println(9/0);
 		System.out.println("---> POST 방식의 요청 처리");
+	}
+	
+	public void destroy() {
+		System.out.println("---> destory() 호출");
 	}
 	
 }
