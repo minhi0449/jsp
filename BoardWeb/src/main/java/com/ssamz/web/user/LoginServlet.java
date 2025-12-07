f1713a248eab6d4fe44b3f4b3662c7e7c05aa7ea
minhi0449 <minhi0449@gmail.com>
2025-12-07 22:39:49 +0900

    LoginServlet

--- BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
+++ BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoginProcess", urlPatterns = { "/LoginProcess" })
+@WebServlet("/login.do")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
